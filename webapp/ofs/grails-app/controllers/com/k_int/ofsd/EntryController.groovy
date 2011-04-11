2848559d59420de391cef99e0c16178892846b64
Ian Ibbotson <ianibbo@gmail.com>
2011-04-11 11:59:38 +0100

    Added switchable ecd/fsd templates

--- webapp/ofs/grails-app/controllers/com/k_int/ofsd/EntryController.groovy
+++ webapp/ofs/grails-app/controllers/com/k_int/ofsd/EntryController.groovy
@@ -31,6 +31,15 @@ class EntryController {
       result['entry'] = target_solr_doc
       def dpp_url = target_solr_doc['repo_url_s']
       println "Got repo url: ${dpp_url}"
+
+      switch ( target_solr_doc['restp'] ) {
+        case 'ServiceProvider':
+          render(view:'ecd',model:result)
+          break;
+        case 'Service':
+          render(view:'fsd',model:result)
+          break;
+      }
     }
 
     result
