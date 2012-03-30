2d2a8435e98293476ff5e84891b19a87d5fca162
ianibo <ianibbo@googlemail.com>
2012-03-30 15:27:05 +0100

    Comment out stats controller

---
+++ testing/analytics/test.groovy
@@ -0,0 +1,9 @@
+
+
+@GrabResolver(name='google-api-services', root='http://mavenrepo.google-api-java-client.googlecode.com/hg')
+// @Grab(group='com.gmongo', module='gmongo', version='0.9.2')
+@Grapes([
+  @Grab(group='com.google.apis', module='google-api-services-analytics', version='v3-rev3-1.4.0-beta'),
+])
+
+
