b46849c30b6eb2dcec27114fd5df1bd4eed81ed2
ianibo <ianibbo@googlemail.com>
2012-03-30 17:21:25 +0100

    Formed request

--- testing/analytics/at.groovy
+++ testing/analytics/at.groovy
@@ -1,7 +1,10 @@
+#!/usr/bin/groovy
+
 @GrabResolver(name='google-api-services', root='http://mavenrepo.google-api-java-client.googlecode.com/hg')
 // @Grab(group='com.gmongo', module='gmongo', version='0.9.2')
 @Grapes([
   @Grab(group='com.google.apis', module='google-api-services-analytics', version='v3-rev3-1.4.0-beta'),
+  @Grab(group='joda-time', module='joda-time', version='2.1')
 ])
 
 import com.google.gdata.client.analytics.AnalyticsService
@@ -16,184 +19,184 @@ import org.joda.time.format.*
 import com.google.gdata.data.analytics.DataEntry
 import org.codehaus.groovy.grails.commons.ConfigurationHolder
 
-def as = new GoogleAnalyticsService()
-as.past30DayVisits();
 
-class GoogleAnalyticsService {
-    def myService
-    static final String ACCOUNTS_URL = "https://www.google.com/analytics/feeds/accounts/default";
-    static final String DATA_URL = "https://www.google.com/analytics/feeds/data";
-    static final String TABLE_ID = "ga:XXXX";
-    static final String USERNAME = "acct"
-    static final String PASSWORD = "pass"
 
-    GoogleAnalyticsService() {
-        try {
-            myService = new AnalyticsService("ksdl-webapp-analytics");
-            if (PASSWORD != "xXxXxXxXxXx") {
-                myService.setUserCredentials(USERNAME, PASSWORD);
-            }
-        } catch(error) {
-            log.error "Failed to log on to Google Analytics for Statistics.", error
+
+def myService
+static final String ACCOUNTS_URL = "https://www.google.com/analytics/feeds/accounts/default";
+static final String DATA_URL = "https://www.google.com/analytics/feeds/data";
+static final String TABLE_ID = "ga:XXXX";
+static final String USERNAME = "acct"
+static final String PASSWORD = "pass"
+
+try {
+  myService = new AnalyticsService("ksdl-webapp-analytics");
+  if (PASSWORD != "xXxXxXxXxXx") {
+    myService.setUserCredentials(USERNAME, PASSWORD);
+  }
+} catch(error) {
+  log.error "Failed to log on to Google Analytics for Statistics.", error
+}
+
+past30DayVisits();
+
+
+
+def past30DayVisits() {
+    def results = []
+    try {
+        URL feedUrl = new URL(ACCOUNTS_URL);
+        AccountFeed feed = myService.getFeed(feedUrl, AccountFeed.class);
+
+        // Print the results of a basic request
+        DataQuery basicQuery = getPastMonthNewVisits(TABLE_ID);
+        DataFeed dataFeed = myService.getFeed(basicQuery, DataFeed.class);
+        for (DataEntry entry : dataFeed.getEntries()) {
+            def value = [date: Date.parse("yyyyMMdd",entry.stringValueOf("ga:date")), visits: [newVisitors:entry.stringValueOf("ga:visitors").toInteger()]]
+            results << value
         }
-    }
 
-    def past30DayVisits() {
-        def results = []
-        try {
-            URL feedUrl = new URL(ACCOUNTS_URL);
-            AccountFeed feed = myService.getFeed(feedUrl, AccountFeed.class);
-
-            // Print the results of a basic request
-            DataQuery basicQuery = getPastMonthNewVisits(TABLE_ID);
-            DataFeed dataFeed = myService.getFeed(basicQuery, DataFeed.class);
-            for (DataEntry entry : dataFeed.getEntries()) {
-                def value = [date: Date.parse("yyyyMMdd",entry.stringValueOf("ga:date")), visits: [newVisitors:entry.stringValueOf("ga:visitors").toInteger()]]
-                results << value
-            }
-
-            basicQuery = getPastMonthReturningVisits(TABLE_ID);
-            dataFeed = myService.getFeed(basicQuery, DataFeed.class);
-            for (DataEntry entry : dataFeed.getEntries()) {
-                def values = results.find{it.date == Date.parse("yyyyMMdd",entry.stringValueOf("ga:date"))}
-                def v1 = values.get('visits')
-                v1.put('returningVisitors', entry.stringValueOf("ga:visitors").toInteger())
-            }
-
-            basicQuery = getPastMonthAllVisits(TABLE_ID);
-            dataFeed = myService.getFeed(basicQuery, DataFeed.class);
-            for (DataEntry entry : dataFeed.getEntries()) {
-                def values = results.find{it.date == Date.parse("yyyyMMdd",entry.stringValueOf("ga:date"))}
-                def v1 = values.get('visits')
-                v1.put('totalVisitors', entry.stringValueOf("ga:visitors").toInteger())
-            }
-        } catch (ServiceException se) {
-            log.error "", se
-        } catch (IOException ioe) {
-            log.error "", ioe
+        basicQuery = getPastMonthReturningVisits(TABLE_ID);
+        dataFeed = myService.getFeed(basicQuery, DataFeed.class);
+        for (DataEntry entry : dataFeed.getEntries()) {
+            def values = results.find{it.date == Date.parse("yyyyMMdd",entry.stringValueOf("ga:date"))}
+            def v1 = values.get('visits')
+            v1.put('returningVisitors', entry.stringValueOf("ga:visitors").toInteger())
         }
-        results
-    }
 
-    def top10Pages() {
-        def topTen = []
-        try {
-            URL feedUrl = new URL(ACCOUNTS_URL);
-            AccountFeed feed = myService.getFeed(feedUrl, AccountFeed.class);
-
-            // Print the results of a basic request
-            DataQuery basicQuery = getTopPagesQuery(TABLE_ID, 10);
-            DataFeed dataFeed = myService.getFeed(basicQuery, DataFeed.class);
-            for (DataEntry entry : dataFeed.getEntries()) {
-                def path = entry.stringValueOf("ga:pagePath")
-                path = path.startsWith("/app") ? path.substring(4) : path
-                def title = entry.stringValueOf("ga:pageTitle")
-                title = title.contains("Kagyu Samye Dzong London: ") ? title.substring(26) : title
-                topTen << [title: title, path: path, visits: entry.stringValueOf("ga:pageviews")]
-            }
-        } catch (ServiceException se) {
-            log.error "", se
-        } catch (IOException ioe) {
-            log.error "", ioe
+        basicQuery = getPastMonthAllVisits(TABLE_ID);
+        dataFeed = myService.getFeed(basicQuery, DataFeed.class);
+        for (DataEntry entry : dataFeed.getEntries()) {
+            def values = results.find{it.date == Date.parse("yyyyMMdd",entry.stringValueOf("ga:date"))}
+            def v1 = values.get('visits')
+            v1.put('totalVisitors', entry.stringValueOf("ga:visitors").toInteger())
         }
-        topTen
+    } catch (ServiceException se) {
+        log.error "", se
+    } catch (IOException ioe) {
+        log.error "", ioe
     }
+    results
+}
 
-    def top5Pages() {
-        def topTen = []
-        try {
-            URL feedUrl = new URL(ACCOUNTS_URL);
-            AccountFeed feed = myService.getFeed(feedUrl, AccountFeed.class);
+def top10Pages() {
+    def topTen = []
+    try {
+        URL feedUrl = new URL(ACCOUNTS_URL);
+        AccountFeed feed = myService.getFeed(feedUrl, AccountFeed.class);
 
-            // Print the results of a basic request
-            DataQuery basicQuery = getTopPagesQuery(TABLE_ID, 5);
-            DataFeed dataFeed = myService.getFeed(basicQuery, DataFeed.class);
-            for (DataEntry entry : dataFeed.getEntries()) {
-                def path = entry.stringValueOf("ga:pagePath")
-                path = path.startsWith("/app") ? path.substring(4) : path
-                topTen << [title: entry.stringValueOf("ga:pageTitle"), path: path, visits: entry.stringValueOf("ga:pageviews")]
-            }
-        } catch (ServiceException se) {
-            log.error "", se
-        } catch (IOException ioe) {
-            log.error "", ioe
+        // Print the results of a basic request
+        DataQuery basicQuery = getTopPagesQuery(TABLE_ID, 10);
+        DataFeed dataFeed = myService.getFeed(basicQuery, DataFeed.class);
+        for (DataEntry entry : dataFeed.getEntries()) {
+            def path = entry.stringValueOf("ga:pagePath")
+            path = path.startsWith("/app") ? path.substring(4) : path
+            def title = entry.stringValueOf("ga:pageTitle")
+            title = title.contains("Kagyu Samye Dzong London: ") ? title.substring(26) : title
+            topTen << [title: title, path: path, visits: entry.stringValueOf("ga:pageviews")]
         }
-        topTen
+    } catch (ServiceException se) {
+        log.error "", se
+    } catch (IOException ioe) {
+        log.error "", ioe
     }
+    topTen
+}
 
-    DataQuery getTopPagesQuery(tableId, numberOfResults) throws MalformedURLException {
-        def now = new java.util.Date()
-        now.clearTime()
-        def dt = new DateTime(now.getTime())
-        DateTime startOfMonth = dt.dayOfMonth().withMinimumValue();
-        DateTime endOfMonth = dt.dayOfMonth().withMaximumValue();
+def top5Pages() {
+    def topTen = []
+    try {
+        URL feedUrl = new URL(ACCOUNTS_URL);
+        AccountFeed feed = myService.getFeed(feedUrl, AccountFeed.class);
 
-        // Set up the request (we could alternately construct a URL manually with all query parameters set)
-        DataQuery query = new DataQuery(new URL(DATA_URL));
-        query.setIds(tableId);
-        query.setStartDate(startOfMonth.toDate().format('yyyy-MM-dd'));
-        query.setEndDate(endOfMonth.toDate().format('yyyy-MM-dd'));
-        query.setDimensions("ga:pageTitle,ga:pagePath");
-        query.setMetrics("ga:pageviews");
-        query.setSort("-ga:pageviews");
-        query.setMaxResults(numberOfResults);
-
-        return query;
+        // Print the results of a basic request
+        DataQuery basicQuery = getTopPagesQuery(TABLE_ID, 5);
+        DataFeed dataFeed = myService.getFeed(basicQuery, DataFeed.class);
+        for (DataEntry entry : dataFeed.getEntries()) {
+            def path = entry.stringValueOf("ga:pagePath")
+            path = path.startsWith("/app") ? path.substring(4) : path
+            topTen << [title: entry.stringValueOf("ga:pageTitle"), path: path, visits: entry.stringValueOf("ga:pageviews")]
+        }
+    } catch (ServiceException se) {
+        log.error "", se
+    } catch (IOException ioe) {
+        log.error "", ioe
     }
+    topTen
+}
 
-    DataQuery getPastMonthNewVisits(tableId) throws MalformedURLException {
-        def now = new java.util.Date()-1
-        now.clearTime()
-        DateTime end = new DateTime(now.getTime())
-        DateTime start = end.minusMonths(1);
+DataQuery getTopPagesQuery(tableId, numberOfResults) throws MalformedURLException {
+    def now = new java.util.Date()
+    now.clearTime()
+    def dt = new DateTime(now.getTime())
+    DateTime startOfMonth = dt.dayOfMonth().withMinimumValue();
+    DateTime endOfMonth = dt.dayOfMonth().withMaximumValue();
 
-        // Set up the request (we could alternately construct a URL manually with all query parameters set)
-        DataQuery query = new DataQuery(new URL(DATA_URL));
-        query.setIds(tableId);
-        query.setStartDate(start.toDate().format('yyyy-MM-dd'));
-        query.setEndDate(end.toDate().format('yyyy-MM-dd'));
-        query.setDimensions("ga:date");
-        query.setMetrics("ga:visitors");
-        query.setSegment("gaid::-2");
-        query.setMaxResults(500);
+    // Set up the request (we could alternately construct a URL manually with all query parameters set)
+    DataQuery query = new DataQuery(new URL(DATA_URL));
+    query.setIds(tableId);
+    query.setStartDate(startOfMonth.toDate().format('yyyy-MM-dd'));
+    query.setEndDate(endOfMonth.toDate().format('yyyy-MM-dd'));
+    query.setDimensions("ga:pageTitle,ga:pagePath");
+    query.setMetrics("ga:pageviews");
+    query.setSort("-ga:pageviews");
+    query.setMaxResults(numberOfResults);
 
-        return query;
-    }
+    return query;
+}
 
-    DataQuery getPastMonthReturningVisits(tableId) throws MalformedURLException {
-        def now = new java.util.Date()-1
-        now.clearTime()
-        DateTime end = new DateTime(now.getTime())
-        DateTime start = end.minusMonths(1);
+DataQuery getPastMonthNewVisits(tableId) throws MalformedURLException {
+    def now = new java.util.Date()-1
+    now.clearTime()
+    DateTime end = new DateTime(now.getTime())
+    DateTime start = end.minusMonths(1);
 
-        // Set up the request (we could alternately construct a URL manually with all query parameters set)
-        DataQuery query = new DataQuery(new URL(DATA_URL));
-        query.setIds(tableId);
-        query.setStartDate(start.toDate().format('yyyy-MM-dd'));
-        query.setEndDate(end.toDate().format('yyyy-MM-dd'));
-        query.setDimensions("ga:date");
-        query.setMetrics("ga:visitors");
-        query.setSegment("gaid::-3");
-        query.setMaxResults(500);
+    // Set up the request (we could alternately construct a URL manually with all query parameters set)
+    DataQuery query = new DataQuery(new URL(DATA_URL));
+    query.setIds(tableId);
+    query.setStartDate(start.toDate().format('yyyy-MM-dd'));
+    query.setEndDate(end.toDate().format('yyyy-MM-dd'));
+    query.setDimensions("ga:date");
+    query.setMetrics("ga:visitors");
+    query.setSegment("gaid::-2");
+    query.setMaxResults(500);
 
-        return query;
-    }
-    DataQuery getPastMonthAllVisits(tableId) throws MalformedURLException {
-        def now = new java.util.Date()-1
-        now.clearTime()
-        DateTime end = new DateTime(now.getTime())
-        DateTime start = end.minusMonths(1);
+    return query;
+}
 
-        // Set up the request (we could alternately construct a URL manually with all query parameters set)
-        DataQuery query = new DataQuery(new URL(DATA_URL));
-        query.setIds(tableId);
-        query.setStartDate(start.toDate().format('yyyy-MM-dd'));
-        query.setEndDate(end.toDate().format('yyyy-MM-dd'));
-        query.setDimensions("ga:date");
-        query.setMetrics("ga:visitors");
-        query.setSegment("gaid::-1");
-        query.setMaxResults(500);
+DataQuery getPastMonthReturningVisits(tableId) throws MalformedURLException {
+    def now = new java.util.Date()-1
+    now.clearTime()
+    DateTime end = new DateTime(now.getTime())
+    DateTime start = end.minusMonths(1);
 
-        return query;
-    }
+    // Set up the request (we could alternately construct a URL manually with all query parameters set)
+    DataQuery query = new DataQuery(new URL(DATA_URL));
+    query.setIds(tableId);
+    query.setStartDate(start.toDate().format('yyyy-MM-dd'));
+    query.setEndDate(end.toDate().format('yyyy-MM-dd'));
+    query.setDimensions("ga:date");
+    query.setMetrics("ga:visitors");
+    query.setSegment("gaid::-3");
+    query.setMaxResults(500);
+
+    return query;
+}
+DataQuery getPastMonthAllVisits(tableId) throws MalformedURLException {
+    def now = new java.util.Date()-1
+    now.clearTime()
+    DateTime end = new DateTime(now.getTime())
+    DateTime start = end.minusMonths(1);
+
+    // Set up the request (we could alternately construct a URL manually with all query parameters set)
+    DataQuery query = new DataQuery(new URL(DATA_URL));
+    query.setIds(tableId);
+    query.setStartDate(start.toDate().format('yyyy-MM-dd'));
+    query.setEndDate(end.toDate().format('yyyy-MM-dd'));
+    query.setDimensions("ga:date");
+    query.setMetrics("ga:visitors");
+    query.setSegment("gaid::-1");
+    query.setMaxResults(500);
+
+    return query;
 }
--- testing/analytics/test.groovy
+++ testing/analytics/test.groovy
@@ -1,9 +1,56 @@
+#!/usr/bin/groovy
 
-
-@GrabResolver(name='google-api-services', root='http://mavenrepo.google-api-java-client.googlecode.com/hg')
+@GrabResolver(name='es', root='https://oss.sonatype.org/content/repositories/releases')
 // @Grab(group='com.gmongo', module='gmongo', version='0.9.2')
 @Grapes([
-  @Grab(group='com.google.apis', module='google-api-services-analytics', version='v3-rev3-1.4.0-beta'),
+  @Grab(group='org.apache.httpcomponents', module='httpmime', version='4.1.2'),
+  @Grab(group='com.gmongo', module='gmongo', version='0.9.2'),
+  @Grab(group='org.apache.httpcomponents', module='httpclient', version='4.0'),
+  @Grab(group='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.5.0'),
+  @Grab(group='org.codehaus.jackson', module='jackson-mapper-asl', version='1.9.6'),
+  @Grab(group='commons-codec', module='commons-codec', version='1.6')
 ])
 
 
+// Following https://developers.google.com/accounts/docs/OAuth2ServiceAccount
+// Useful info from http://www.mkyong.com/java/java-sha-hashing-example/ 
+// OAuth flow here: https://developers.google.com/accounts/docs/OAuth2#scenarios
+
+import org.codehaus.jackson.map.ObjectMapper
+import org.apache.commons.codec.binary.Base64
+import java.security.MessageDigest;
+
+// Build the assertion
+def jwt_header = ["alg":"RS256","typ":"JWT"]
+def required_claims = [
+  "iss":"230924794467@developer.gserviceaccount.com",
+  "scope":"https://www.googleapis.com/auth/analytics",
+  "aud":"https://accounts.google.com/o/oauth2/token",
+  "exp":1328554385,
+  "iat":1328550785
+]
+
+ObjectMapper mapper = new ObjectMapper();
+StringWriter sw = new StringWriter();
+mapper.writeValue(sw, jwt_header);
+def jwt_header_string = sw.toString()
+
+sw = new StringWriter();
+mapper.writeValue(sw, required_claims);
+def required_claims_string = sw.toString()
+
+println("map: ${jwt_header_string} ${required_claims_string}");
+
+// We post the assertion to https://accounts.google.com/o/oauth2/token
+
+String encoded_header = new String(Base64.encodeBase64(jwt_header_string.getBytes()));
+String encoded_required_claims = new String(Base64.encodeBase64(required_claims_string.getBytes()));
+
+String jwt_to_sign="${encoded_header}.${encoded_required_claims}"
+
+MessageDigest md = MessageDigest.getInstance("SHA-256");
+md.update(encoded_required_claims.getBytes())
+String digestHex = new BigInteger(1, md.digest()).toString(16);
+
+println("Final header: ${encoded_header} ${encoded_required_claims}");
+println("Final req: ${jwt_to_sign}.${digestHex}");
