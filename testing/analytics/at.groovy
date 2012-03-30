97103d3f3fc7e14a08d34263a14b4c4207960936
ianibo <ianibbo@googlemail.com>
2012-03-30 15:42:21 +0100

    Added grape config

--- testing/analytics/at.groovy
+++ testing/analytics/at.groovy
@@ -1,4 +1,8 @@
-package org.samye.dzong.london.google
+@GrabResolver(name='google-api-services', root='http://mavenrepo.google-api-java-client.googlecode.com/hg')
+// @Grab(group='com.gmongo', module='gmongo', version='0.9.2')
+@Grapes([
+  @Grab(group='com.google.apis', module='google-api-services-analytics', version='v3-rev3-1.4.0-beta'),
+])
 
 import com.google.gdata.client.analytics.AnalyticsService
 import com.google.gdata.util.ServiceException
